c052b110c5a5fb0f027f64dadc4d4647a5f97a1e
Brett Slaski <brettski@yahoo.com>
2024-01-17 19:50:55 -0600

    chore: setup project for .net 4.8 and generated new nuget file

--- PasswordGenerator/Properties/AssemblyInfo.cs
+++ PasswordGenerator/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BD Password Generator")]
-[assembly: AssemblyDescription("Password Generation Library http://bdpasswordgenerator.codeplex.com")]
+[assembly: AssemblyDescription("Fast, flexible and lightweight password and random string generation library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brettski")]
-[assembly: AssemblyProduct("PasswordGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Brettski PasswordGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2013, 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
